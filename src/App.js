02155d5cabfac56172f4e09f1e5aac8fb7936d75
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-14 16:53:57 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import Accordion from "./Accordion";
 import "./App.css";
-import { questions } from "./questions";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>FAQ</h1>
-      <div className="accordion">
-        {questions.map(({ title, answer }) => (
-          <Accordion title={title} answer={answer} />
-        ))}
-      </div>
+      
     </div>
   );
 }
